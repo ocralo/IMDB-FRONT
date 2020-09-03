01c6bb86dd44568021b1ba9d78ea9d360be8bbc6
ocralo <ocralo@gmail.com>
2020-09-02 22:00:22 -0500

    se acomodan los estilos de la card y el redireccionamiento a la vista de la serie

--- src/Components/Components/CardSerie/CardSerie.js
+++ src/Components/Components/CardSerie/CardSerie.js
@@ -2,26 +2,55 @@ import React from "react";
 
 //importacion de librerias externas
 import { Card } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 /**
  *
  * @param {*} param0
  */
-export default function CardSerie({ title = "titulo", imageURL }) {
+export default function CardSerie({
+	title = "titulo",
+	imageURL,
+	vote_average,
+	idSerie,
+}) {
+	let history = useHistory();
+
+	function handleDoubleClick() {
+		history.push(`/Series/${idSerie}`);
+	}
 	return (
-		<Card className="shadow mt-4">
-			<Card.Img
-				variant="top"
-				src={
-					imageURL
-						? `https://image.tmdb.org/t/p/w500${imageURL}`
-						: `${process.env.PUBLIC_URL}/img/error.svg`
-				}
-				height="167.453"
-			/>
-			<Card.Body className="p-2">
-				<Card.Title className="m-0">{title}</Card.Title>
-			</Card.Body>
-		</Card>
+		<div
+			className="flip-card mt-4"
+			onDoubleClick={handleDoubleClick}>
+			<div class="flip-card-inner">
+				<Card className="shadow flip-card-front">
+					<Card.Img
+						variant="top"
+						src={
+							imageURL
+								? `https://image.tmdb.org/t/p/w500${imageURL}`
+								: `${process.env.PUBLIC_URL}/img/error.svg`
+						}
+						height="167.453"
+					/>
+					<Card.Body className="p-2">
+						<Card.Title className="m-0">
+							{title}
+						</Card.Title>
+					</Card.Body>
+				</Card>
+				<Card className="shadow flip-card-back">
+					<Card.Body className="p-2">
+						<Card.Title className="m-0">
+							{title}
+						</Card.Title>
+						<Card.Text className="mt-2">
+							Rating : {vote_average}
+						</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+		</div>
 	);
 }
