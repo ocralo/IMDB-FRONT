74637e767077beb2d74cad1d2ff261df4fd775a9
ocralo <ocralo@gmail.com>
2020-09-03 19:23:37 -0500

    Updates

---
+++ precache-manifest.3c1dbff5dee1e214a1cc843b0c968c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a2347d929278816a2fede1d8a02a22",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c0a08a370b6aed29ae2",
+    "url": "./static/css/main.da555998.chunk.css"
+  },
+  {
+    "revision": "3cb842ab9b177c3d7390",
+    "url": "./static/js/2.dcd2d167.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.dcd2d167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c0a08a370b6aed29ae2",
+    "url": "./static/js/main.fbe97916.chunk.js"
+  },
+  {
+    "revision": "18153f87b3a0c376b258",
+    "url": "./static/js/runtime-main.090f8b19.js"
+  }
+]);
